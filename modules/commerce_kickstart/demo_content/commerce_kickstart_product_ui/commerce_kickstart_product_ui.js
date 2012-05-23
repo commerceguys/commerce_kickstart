7be787ebd4261f18a736f0070148298f998fd626
Theo <theo@commerceguys.com>
2012-05-23 15:43:13 +0200

    Fix read_more js. See #8672
    Change-Id: Ie36da0d3ca7542ce6c362fbebec65258b03694e8
    Reviewed-on: https://code.commerceguys.com/3777
    Reviewed-by: Hudson bot <bender@commerceguys.com>
    Reviewed-by: Bojan Zivanovic <bojan@commerceguys.com>

--- modules/commerce_kickstart/demo_content/commerce_kickstart_product_ui/commerce_kickstart_product_ui.js
+++ modules/commerce_kickstart/demo_content/commerce_kickstart_product_ui/commerce_kickstart_product_ui.js
@@ -14,15 +14,11 @@
   // Add 'read more' link on description.
   Drupal.behaviors.bodyReadMore = {
     attach: function ( context, settings ) {
-      $.expander.defaults.slicePoint = 120;
-
-      $(document).ready(function() {
-        $('.field-name-body p').expander({
-          slicePoint:       200,
-          expandPrefix:     ' ...<br />',
-          expandText:       'Read more',
-          userCollapseText: 'Close'
-        });
+      $('.field-name-body .field-item.even').expander({
+        slicePoint:       200,
+        expandPrefix:     ' ... ',
+        expandText:       'read more',
+        userCollapseText: 'Close'
       });
     }
   }
