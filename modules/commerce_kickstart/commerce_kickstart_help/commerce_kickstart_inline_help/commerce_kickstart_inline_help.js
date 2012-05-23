56ef091a349ff9de6010f8a51e71c9ff6bbe2492
Nicolas Meyer <nicolas@commerceguys.com>
2012-05-23 14:06:09 +0200

    Major rewrite of the JS used in kickstart inject help.
    Change-Id: Id1eb505936ae9a89d5b92e92bc8db1bf37a5b174
    Reviewed-on: https://code.commerceguys.com/3774
    Reviewed-by: Hudson bot <bender@commerceguys.com>
    Reviewed-by: Bojan Zivanovic <bojan@commerceguys.com>

--- modules/commerce_kickstart/commerce_kickstart_help/commerce_kickstart_inline_help/commerce_kickstart_inline_help.js
+++ modules/commerce_kickstart/commerce_kickstart_help/commerce_kickstart_inline_help/commerce_kickstart_inline_help.js
@@ -1,10 +1,51 @@
 (function ($) {
 
-  $(document).ready(function () {
+  $(function () {
+    Drupal.ajax.prototype.commands.commerce_kickstart_inline_help_open = Drupal.KickstartHelp.Kickstart_display_open;
+    Drupal.ajax.prototype.commands.commerce_kickstart_inline_help_close = Drupal.KickstartHelp.Kickstart_display_close;
+  });
 
-    $('.kickstart-help-full-content').before($('<p><a href="#" class="help-show-more">Show more</a></p>'));
+  // Make sure our objects are defined.
+  Drupal.KickstartHelp = Drupal.KickstartHelp || {};
+
+  /**
+   * AJAX responder command.
+   */
+  Drupal.KickstartHelp.Kickstart_display_open = function (ajax, response, status) {
+
+    textArea = $('#commerce-kickstart-inline-help');
+    openButton = $('#commerce-kickstart-inline-help-button #edit-commerce-kickstart-inline-help-button');
+    openButtonContainer = $('#commerce-kickstart-inline-help-button');
+    closeButton = $('#edit-commerce-kickstart-inline-help-close-button');
+
+    textArea.show('slow');
+    openButtonContainer.hide();
+    closeButton.show();
+    Drupal.KickstartHelp.show();
+    Drupal.attachBehaviors();
+  }
+  /**
+   * AJAX responder command.
+   */
+  Drupal.KickstartHelp.Kickstart_display_close = function (ajax, response, status) {
+
+    textArea = $('#commerce-kickstart-inline-help');
+    openButton = $('#commerce-kickstart-inline-help-button #edit-commerce-kickstart-inline-help-button');
+    openButtonContainer = $('#commerce-kickstart-inline-help-button');
+    closeButton = $('#edit-commerce-kickstart-inline-help-close-button');
+
+    textArea.hide('slow');
+    openButtonContainer.show();
+    closeButton.hide();
+    Drupal.attachBehaviors();
+  }
+
+  Drupal.KickstartHelp.show = function() {
+    if (!$('body').find('.help-show-more').length > 0) {
+      $('.kickstart-help-full-content').before($('<p><a href="#" class="help-show-more">Show more</a></p>'));
+    }
     $('.kickstart-help-full-content').hide();
-    $('.help-show-more').click(function() {
+    $('.help-show-more').click(function () {
       $('.kickstart-help-full-content').toggle('slow');
       if ($('.help-show-more').html() == 'Show more') {
         $('.help-show-more').html('Show less');
@@ -14,36 +55,19 @@
       }
     });
     // Check if the help section is hidden. If so, hide the open button.
-    if ($('#commerce-kickstart-inline-help').is(":visible")) {
-      $('#commerce-kickstart-inline-help-button').hide();
-    }
-  });
+      if ($('#commerce-kickstart-inline-help').is(":visible")) {
+        $('#commerce-kickstart-inline-help-button').hide();
+      }
 
-  Drupal.behaviors.KickstartInlineHelp = {
-    attach:function () {
+  };
 
-      var textArea;
-      var openButton;
-      var openButtonContainer;
-      var closeButton;
-
-      textArea = $('#commerce-kickstart-inline-help');
-      openButton = $('#commerce-kickstart-inline-help-button #edit-commerce-kickstart-inline-help-button');
-      openButtonContainer = $('#commerce-kickstart-inline-help-button');
-      closeButton = $('#edit-commerce-kickstart-inline-help-close-button');
-
-      closeButton.click(function () {
-        textArea.hide('slow');
-        openButtonContainer.show();
-        closeButton.hide();
-      });
-
-      openButton.click(function() {
-        textArea.show('slow');
-        openButtonContainer.hide();
-        closeButton.show();
+  Drupal.behaviors.KickstartHelp = {
+    attach:function (context) {
+      $('#commerce-kickstart-inline-help').once('help-processed', function() {
+        Drupal.KickstartHelp.show();
       });
     }
-  }
+  };
+
 })(jQuery);
 
