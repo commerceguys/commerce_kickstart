728dffffd589add8c5ce4b31608e4800595b8480
Aaron Dudenhofer <dudenhofer@gmail.com>
2012-05-29 13:26:20 -0400

    fixing select box issue
    Change-Id: Ie32f3fadad569abd0cb63efaee4a00a721d569f4
    Reviewed-on: https://code.commerceguys.com/3817
    Reviewed-by: Hudson bot <bender@commerceguys.com>
    Reviewed-by: Damien Tournoud <damien@commerceguys.com>

--- themes/commerce_kickstart_admin/js/script.js
+++
@@ -1,7 +0,0 @@
-(function ($) {
-  Drupal.behaviors.customSelect = {
-    attach: function(context) {
-      $("select.form-select").not(".draggable select.form-select, #views-ajax-popup select.form-select").customSelect();
-    }
-  }
-})(jQuery);
