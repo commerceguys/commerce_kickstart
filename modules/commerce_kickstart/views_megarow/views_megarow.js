43a897f3d0f8302505d16d1778107f9b96488e9c
Bojan Zivanovic <bojanz@gmail.com>
2012-06-07 13:53:34 +0200

    Update views_megarow to latest sandbox code, replace commerce_product_manager and commerce_order_manager with commerce_backoffice from the sandbox.
    Change-Id: Ibd6f4b53fd4bd83db2189d7ff066b0b4ccdae603
    Reviewed-on: https://code.commerceguys.com/3894
    Reviewed-by: Bojan Zivanovic <bojan@commerceguys.com>

--- modules/commerce_kickstart/views_megarow/views_megarow.js
+++ modules/commerce_kickstart/views_megarow/views_megarow.js
@@ -1,11 +1,11 @@
 /**
  * @file
  *
- * Alternative implementation of the CTools modal form.
- * Opens a "megarow" in the table instead of a dialog.
+ * Provides modal-like functionality that opens a "megarow" in a table
+ * instead of opening a dialog. Multiple megarows can be open at the same time,
+ * each inserted below the triggering (parent) row.
  *
- * Unlike with CTools modals, multiple megarows can be open at the same time,
- * which is why each command accepts an id used to match the correct megarow.
+ * Inspired by CTools modal.
  *
  * This javascript relies on the CTools ajax responder.
  */
@@ -14,88 +14,93 @@
   Drupal.ViewsMegarow = Drupal.ViewsMegarow || {};
 
   /**
-   * Display the modal
+   * Display the megarow.
    */
-  Drupal.ViewsMegarow.show = function(entity_id) {
+  Drupal.ViewsMegarow.open = function(entityId) {
     // There's already a megarow open for this entity, abort.
-    if ($(Drupal.ViewsMegarow.getRow(entity_id)).length > 0) {
+    if ($('.views-megarow-content-' + entityId).length > 0) {
       return;
     }
 
     var defaults = {
-      modalTheme: 'ViewsMegarowDialog',
+      megarowTheme: 'ViewsMegarowDialog',
       throbberTheme: 'ViewsMegarowThrobber',
       animation: 'show',
       animationSpeed: 'fast',
-      modalOptions: {
-        opacity: .55,
-        background: '#fff'
-      }
     };
     var settings = {};
     $.extend(true, settings, defaults, Drupal.settings.ViewsMegarow);
     Drupal.ViewsMegarow.currentSettings = settings;
 
-    var megarowContent = $(Drupal.theme(settings.modalTheme, entity_id));
-
+    // Get the megarow HTML, add the "Loading" title and animation.
+    var megarowContent = $(Drupal.theme(settings.megarowTheme, entityId));
     $('.megarow-title', megarowContent).html(Drupal.ViewsMegarow.currentSettings.loadingText);
     $('.megarow-content', megarowContent).html(Drupal.theme(settings.throbberTheme));
     megarowContent.hide();
 
     // Create our megarow.
-    var tableclass = $(".views-table").attr('class');
-    var nbcols = tableclass.replace('views-table cols-','');
-    $('tr.item-' + entity_id).after('<tr class="megarow-content"><td class="megarow-cell" colspan="' + nbcols + '"><div class="modalContent">' + $(megarowContent).html() + '</div></td></tr>');
+    var nbcols = $(".views-table").attr('class').replace('views-table cols-', '');
+    var wrapper_html = '';
+    wrapper_html += '<tr class="megarow">';
+    wrapper_html += '  <td colspan="' + nbcols + '">';
+    wrapper_html += '  <div class="views-megarow-content views-megarow-content-' + entityId + '">';
+    wrapper_html +=      $(megarowContent).html();
+    wrapper_html += '   </div>';
+    wrapper_html += '  </td>';
+    wrapper_html += '</tr>;'
+    $('tr.item-' + entityId).after(wrapper_html);
+    // Mark the parent row as active.
+    $('tr.item-' + entityId).addClass('views-row-active');
 
-    // Bind a click for closing the modalContent
-    modalContentClose = function(event){
-      close(event);
-      return false;
-    };
-    $(Drupal.ViewsMegarow.getRow(entity_id)).find('.close').bind('click', modalContentClose);
+    // Get the megarow from the DOM, now that it's been inserted.
+    var megarow = $('.views-megarow-content-' + entityId);
 
-    // Close the open modal content and backdrop
-    function close(event) {
-      var megarow = $(event.target).parents('tr.megarow-content');
-
-      // Unbind the events
-      $(event.target).unbind('click', modalContentClose);
-      $(document).trigger('CToolsDetachBehaviors', megarow);
-
-      // Set our animation parameters and use them
-      if (settings.animation == 'fadeIn') animation = 'fadeOut';
-      if (settings.animation == 'slideDown') animation = 'slideUp';
-      if (settings.animation == 'show') animation = 'hide';
-
-      // Close and remove the megarw
-      $('.modalContent', megarow).hide()[animation](settings.speed);
-      $(megarow).remove();
-    };
+    // Bind a click for closing the megarow.
+    $('.close', megarow).bind('click', { entityId: entityId }, function(event) {
+      Drupal.ViewsMegarow.close(event.data.entityId);
+    });
   };
 
   /**
-   * Helper to get the megarow matching the passed entity id.
+   * Close the megarow.
    */
-  Drupal.ViewsMegarow.getRow = function (entity_id) {
-    return $('.views-megarow-content-' + entity_id).parents('tr').eq(0);
+  Drupal.ViewsMegarow.close = function(entityId) {
+    var megarow = $('.views-megarow-content-' + entityId).parents('tr.megarow');
+
+    // Unbind the events.
+    $(document).trigger('CToolsDetachBehaviors', megarow);
+
+    // Set our animation parameters and use them.
+    var animation = Drupal.ViewsMegarow.currentSettings.animation;
+    if (animation == 'fadeIn') {
+      animation = 'fadeOut';
+    }
+    else if (animation == 'slideDown') {
+      animation = 'slideUp';
+    }
+    else {
+      animation = 'hide';
+    }
+
+    // Close and remove the megarow.
+    $('.views-megarow-content', megarow).hide()[animation](Drupal.ViewsMegarow.currentSettings.animationSpeed);
+    megarow.remove();
+
+    // Mark the parent row as inactive.
+    $('tr.item-' + entityId).removeClass('views-row-active');
   }
 
   /**
    * Provide the HTML to create the megarow.
    */
-  Drupal.theme.prototype.ViewsMegarowDialog = function (entity_id) {
+  Drupal.theme.prototype.ViewsMegarowDialog = function (entityId) {
     var html = '';
-    html += '<div class="views-megarow" class="popups-box">';
-    html += '  <div class="views-megarow-content views-megarow-content-' + entity_id + '">';
-    html += '    <div class="popups-container">';
-    html += '      <div class="megarow-header clearfix">';
-    html += '        <span class="megarow-title" class="megarow-title"></span>';
-    html += '        <span class="popups-close"><a class="close" href="#">' + Drupal.ViewsMegarow.currentSettings.closeText + '</a></span>';
-    html += '        <div class="clear-block"></div>';
-    html += '      </div>';
-    html += '      <div class="megarow-scroll"><div class="megarow-content" class="megarow-content popups-body"></div></div>';
+    html += '<div>'; // This div doesn't get inserted into a DOM.
+    html += '  <div class="megarow-header clearfix">';
+    html += '    <span class="megarow-title"></span>';
+    html += '      <a class="close" href="#">' + Drupal.ViewsMegarow.currentSettings.close + '</a>';
     html += '    </div>';
-    html += '  </div>';
+    html += '   <div class="megarow-content"></div>';
     html += '</div>';
     return html;
   }
@@ -118,8 +123,8 @@
    * Handler to prepare the megarow for the response
    */
   Drupal.ViewsMegarow.clickAjaxLink = function () {
-    var entity_id = $(this).parents('tr').attr('data-entity-id');
-    Drupal.ViewsMegarow.show(entity_id);
+    var entityId = $(this).parents('tr').attr('data-entity-id');
+    Drupal.ViewsMegarow.open(entityId);
 
     return false;
   };
@@ -148,14 +153,14 @@
         .click(Drupal.ViewsMegarow.clickAjaxLink)
         .each(function () {
           // Create a drupal ajax object
-          var element_settings = {};
+          var elementSettings = {};
           if ($(this).attr('href')) {
-            element_settings.url = $(this).attr('href');
-            element_settings.event = 'click';
-            element_settings.progress = { type: 'throbber' };
+            elementSettings.url = $(this).attr('href');
+            elementSettings.event = 'click';
+            elementSettings.progress = { type: 'throbber' };
           }
           var base = $(this).attr('href');
-          Drupal.ajax[base] = new Drupal.ajax(base, this, element_settings);
+          Drupal.ajax[base] = new Drupal.ajax(base, this, elementSettings);
         }
       );
 
@@ -163,13 +168,13 @@
       $('.megarow-content form:not(.views-megarow-open-processed)')
         .addClass('views-megarow-open-processed')
         .each(function() {
-          var element_settings = {};
-          element_settings.url = $(this).attr('action');
-          element_settings.event = 'submit';
-          element_settings.progress = { 'type': 'throbber' }
+          var elementSettings = {};
+          elementSettings.url = $(this).attr('action');
+          elementSettings.event = 'submit';
+          elementSettings.progress = { 'type': 'throbber' }
           var base = $(this).attr('id');
 
-          Drupal.ajax[base] = new Drupal.ajax(base, this, element_settings);
+          Drupal.ajax[base] = new Drupal.ajax(base, this, elementSettings);
           Drupal.ajax[base].form = $(this);
 
           $('input[type=submit], button', this).click(function() {
@@ -183,125 +188,55 @@
   // The following are implementations of AJAX responder commands.
 
   /**
-   * AJAX responder command to place HTML within the modal.
+   * AJAX responder command to place HTML within the megarow.
    */
   Drupal.ViewsMegarow.megarow_display = function(ajax, response, status) {
-    if ($(Drupal.ViewsMegarow.getRow(response.entity_id)).find('.modalContent').length == 0) {
-      Drupal.ViewsMegarow.show(response.entity_id);
-    };
-    Drupal.ViewsMegarow.getRow(response.entity_id).find('.megarow-title').html(response.title);
-    Drupal.ViewsMegarow.getRow(response.entity_id).find('.megarow-content').html(response.output);
+    var megarow = $('.views-megarow-content-' + response.entity_id);
+
+    Drupal.ViewsMegarow.open(response.entity_id);
+    $('.megarow-title', megarow).html(response.title);
+    $('.megarow-content', megarow).html(response.output);
     Drupal.attachBehaviors();
   }
 
   /**
-   * AJAX responder command to dismiss the modal.
-   * @TODO: Find an example to use the dismiss part and test it.
+   * AJAX responder command to dismiss the megarow.
    */
   Drupal.ViewsMegarow.megarow_dismiss = function(ajax, response, status) {
-    var content = Drupal.ViewsMegarow.getRow(response.entity_id).find('.modalContent');
-
-    // If our animation isn't "fadeIn" or "slideDown" then it always is show
-    if (animation != 'fadeOut' && animation != 'slideUp') {
-      animation = 'show';
-    }
-
-    // Unbind the events we bound
-   // $('.close').unbind('click', modalContentClose);
-    $(document).trigger('CToolsDetachBehaviors', content);
-
-    // jQuery magic loop through the instances and run the animations or removal.
-    content.each(function(){
-      if (animation == 'fade') {
-        Drupal.ViewsMegarow.getRow().find('.modalContent').fadeOut(speed,function(){$(this).remove();});
-      }
-      else {
-        if (animation == 'slide') {
-          Drupal.ViewsMegarow.getRow().find('.modalContent').slideUp(speed,function(){$(this).remove();});
-        }
-        else {
-          Drupal.ViewsMegarow.getRow().find('.modalContent').remove();
-        }
-      }
-    });
+    Drupal.ViewsMegarow.close(response.entity_id);
   }
 
   /**
-   * Find a URL for an AJAX button.
-   *
-   * The URL for this gadget will be composed of the values of items by
-   * taking the ID of this item and adding -url and looking for that
-   * class. They need to be in the form in order since we will
-   * concat them all together using '/'.
+   * AJAX responder command to refresh the parent row of a megarow.
    */
-  Drupal.ViewsMegarow.findURL = function(item) {
-    var url = '';
-    var url_class = '.' + $(item).attr('id') + '-url';
-    $(url_class).each(
-      function() {
-        if (url && $(this).val()) {
-          url += '/';
+  Drupal.ViewsMegarow.megarow_refresh_parent = function(ajax, response, status) {
+    // No row found, nothing to update.
+    if ($('tr.item-' + response.entity_id).length == 0) {
+      return;
+    }
+
+    // Fetch the current page using ajax, and extract the relevant data.
+    var table = $('tr.item-' + response.entity_id).parents('table');
+    var viewName = table.attr('data-view-name');
+    var display = table.attr('data-view-display');
+    var url = Drupal.settings.basePath + 'views_megarow/refresh/' + viewName + '/' + display + '/' + response.entity_id;
+
+    $.get(url, function(data) {
+      $('tr.item-' + response.entity_id + ' td', data).each(function(index) {
+        // Ignore cells that contain form elements.
+        if ($('input', this).length == 0 && $('select', this).length == 0) {
+          var targetElement = $('tr.item-' + response.entity_id + ' td:eq(' + index + ')');
+          var newContent = $(this).html();
+          targetElement.html(newContent);
+          Drupal.attachBehaviors(targetElement);
         }
-        url += $(this).val();
       });
-    return url;
-  };
-
-  /**
-   * Add an extra function to the Drupal ajax object
-   * which allows us to trigger an ajax response without
-   * an element that triggers it.
-   */
-  Drupal.ajax.prototype.updateRow = function(url) {
-    var ajax = this;
-
-    ajax.url = url;
-    ajax.options.url = url;
-
-    // Do not perform another ajax command if one is already in progress.
-    if (ajax.ajaxing) {
-      return false;
-    }
-
-    try {
-      $.ajax(ajax.options);
-    }
-    catch (err) {
-      alert('An error occurred while attempting to process ' + ajax.options.url);
-      return false;
-    }
-
-    return false;
-  };
-
-  /**
-   * Define a custom ajax action not associated with an element.
-   */
-  var custom_settings = {};
-  // fake url
-  custom_settings.url = '/views_megarow_update/';
-  custom_settings.keypress = false;
-  custom_settings.prevent = false;
-  Drupal.ajax['custom_ajax_action'] = new Drupal.ajax(null, $(document.body), custom_settings);
-
-  /**
-   * Define a point to trigger our custom actions. e.g. on page load.
-   */
-  Drupal.behaviors.viewsMegarowRefresh = {
-    attach: function (context) {
-      // bind custom event on a row that will refresh it when form is submit with success
-      $('table.views-table tr').bind('refreshRow', function() {
-        var viewName = $(this).parents('table.views-table').attr('data-viewname');
-        var viewDisplay = $(this).parents('table.views-table').attr('data-viewdisplay');
-        var entityId = $(this).attr('data-entity-id');
-        var url = '/views_megarow_update/' + entityId + '/ajax/' + viewName + '/' + viewDisplay;
-        Drupal.ajax['custom_ajax_action'].updateRow(url);
-      });
-    }
-  };
+    });
+  }
 
   $(function() {
     Drupal.ajax.prototype.commands.megarow_display = Drupal.ViewsMegarow.megarow_display;
     Drupal.ajax.prototype.commands.megarow_dismiss = Drupal.ViewsMegarow.megarow_dismiss;
+    Drupal.ajax.prototype.commands.megarow_refresh_parent = Drupal.ViewsMegarow.megarow_refresh_parent;
   });
 })(jQuery);
