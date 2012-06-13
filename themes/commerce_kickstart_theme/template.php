7fbec525863bb383f97769f7352a23f969b54d56
Mickael Mao <mickael@commerceguys.com>
2012-06-13 11:17:23 +0200

    Handle responsive images.
    Change-Id: I87a4e6dce1ed328fa817497070680e445670a02f
    Reviewed-on: https://code.commerceguys.com/3973
    Build: Hudson bot <bender@commerceguys.com>
    Reviewed-by: Bojan Zivanovic <bojan@commerceguys.com>

--- themes/commerce_kickstart_theme/template.php
+++ themes/commerce_kickstart_theme/template.php
@@ -1,17 +1,14 @@
 <?php
-/*
-function commerce_kickstart_theme_form_views_exposed_form_alter(&$form, &$form_state, $form_id) {
-  if ($form['#id'] == 'views-exposed-form-display-products-page') {
-    dsm($form);
-    //$form['#info']['search_api_views_fulltext']['#title'] = t('Search'); // Change the text on the label element
-    $form['#info']['filter-search_api_views_fulltext']['label']['#label_display'] = 'hidden';
-    $form['search_block_form']['#size'] = 40;  // define size of the textfield
-    $form['search_api_views_fulltext']['#default_value'] = t('Search'); // Set a default value for the textfield
-    $form['submit']['#value'] = t('Ok'); // Change the text on the submit button
-    $form['submit'] = array('#type' => 'image_button', '#src' => base_path() . path_to_theme() . '/images/search-button.png');
 
-    // Add extra attributes to the text box
-    $form['search_api_views_fulltext']['#attributes']['onblur'] = "if (this.value == '') {this.value = 'Search';}";
-    $form['search_api_views_fulltext']['#attributes']['onfocus'] = "if (this.value == 'Search') {this.value = '';}";
-  }
-}*/
+function commerce_kickstart_theme_preprocess_html(&$vars) {
+  // Ad respond.js to the footer to avoid crashes in IE8
+  drupal_add_js(drupal_get_path('theme', 'commerce_kickstart_theme') . '/js/respond.js',
+  array(
+    'type' => 'file',
+    'scope' => 'footer',
+    'group' => JS_THEME,
+    'preprocess' => TRUE,
+    'cache' => TRUE,
+  )
+  );
+}
