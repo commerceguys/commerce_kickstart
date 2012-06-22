a4234dfbaf3f54f2a9afdf4fd32659a03a58b326
Mickael Mao <mickael@commerceguys.com>
2012-06-22 17:39:39 +0200

    Theming IE7,8 fixes.
    Change-Id: I03a0d597ed286f75aded7eb6fd0bdcc8907efead
    Reviewed-on: https://code.drupalcommerce.org/6
    Tested-by: Jenkins <jenkins@commerceguys.com>
    Reviewed-by: Bojan Zivanovic <bojan@commerceguys.com>

--- themes/commerce_kickstart_theme/template.php
+++ themes/commerce_kickstart_theme/template.php
@@ -1,1 +1,13 @@
 <?php
+
+/**
+ * Preprocess variables for html.tpl.php
+ *
+ * @see system_elements()
+ * @see html.tpl.php
+ */
+function commerce_kickstart_theme_preprocess_html(&$variables) {
+  // Add conditional stylesheets for IE
+  drupal_add_css(path_to_theme() . '/css/ie-lte-8.css', array('group' => CSS_THEME, 'weight' => 20, 'browsers' => array('IE' => 'lte IE 8', '!IE' => FALSE), 'preprocess' => FALSE));
+  drupal_add_css(path_to_theme() . '/css/ie-lte-7.css', array('group' => CSS_THEME, 'weight' => 21, 'browsers' => array('IE' => 'lte IE 7', '!IE' => FALSE), 'preprocess' => FALSE));
+}
