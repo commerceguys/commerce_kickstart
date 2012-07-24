278e6785af6161b2117a9fba6748ae941484b004
Mickael Mao <mickael@commerceguys.com>
2012-07-24 18:12:36 +0200

    Issue #1697180: Add shadow effect to checkout dialog.
    Change-Id: I3b0a77cf3ba63e30009aa5bc225a9164cfef1e14
    Reviewed-on: https://code.drupalcommerce.org/213
    Tested-by: Jenkins <jenkins@commerceguys.com>
    Reviewed-by: Bojan Zivanovic <bojan@commerceguys.com>

--- themes/commerce_kickstart_theme/js/commerce_kickstart_theme_custom.js
+++ themes/commerce_kickstart_theme/js/commerce_kickstart_theme_custom.js
@@ -9,4 +9,15 @@
       }
     }
   }
+  // Add background overlay to add to cart popin.
+  Drupal.behaviors.commerce_kickstart_add_to_cart_overlay = {
+    attach:function (context, settings) {
+      if ($('.commerce-kickstart-add-to-cart').length > 0 ) {
+        $('body').append("<div class=\"commerce_kickstart_add_to_cart_overlay\"></div>");
+        $('.commerce-kickstart-add-to-cart-close').live('click', function() {
+          $('.commerce_kickstart_add_to_cart_overlay').remove();
+        });
+      }
+    }
+  }
 })(jQuery);
