f60f70e45e1b9443d4e6266c444b8ec39fb399cd
Theo <theo@commerceguys.com>
2012-07-24 15:52:53 +0200

    Issue #1691550 : Product display type not respecting comments settings
    Change-Id: I910abee2c69f49e4ed7517b8dcb1644a4cd91c49
    Reviewed-on: https://code.drupalcommerce.org/211
    Tested-by: Jenkins <jenkins@commerceguys.com>
    Reviewed-by: Bojan Zivanovic <bojan@commerceguys.com>

--- modules/commerce_kickstart/demo_content/commerce_kickstart_lite_product_ui/theme/node--product--type.tpl.php
+++ modules/commerce_kickstart/demo_content/commerce_kickstart_lite_product_ui/theme/node--product--type.tpl.php
@@ -15,20 +15,25 @@
 </div>
   <div class="container-24 grid-8 prefix-1">
     <div class="product-title"><?php print $title; ?></div>
-  <div<?php print $content_attributes; ?>>
-    <?php
-      // We hide the comments and links now so that we can render them later.
-      hide($content['comments']);
-      hide($content['links']);
-      print render($content);
-    ?>
-  </div>
-  
-  <div class="clearfix">
-    <?php if (!empty($content['links'])): ?>
-      <nav class="links node-links clearfix"><?php print render($content['links']); ?></nav>
-    <?php endif; ?>
+    <div<?php print $content_attributes; ?>>
+      <?php
+        // We hide the comments and links now so that we can render them later.
+        hide($content['comments']);
+        hide($content['links']);
+        print render($content);
+      ?>
+    </div>
 
+    <div class="clearfix">
+      <?php if (!empty($content['links'])): ?>
+        <nav class="links node-links clearfix"><?php print render($content['links']); ?></nav>
+      <?php endif; ?>
+    </div>
+  </div>
+  <div class="container-24 grid-24">
+    <?php
+    print render($content['comments']);
+    ?>
   </div>
 </div>
 </article>
