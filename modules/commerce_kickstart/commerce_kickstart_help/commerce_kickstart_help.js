d45800c9bc2493d74ddd47cfc082c7c3da274f4d
Theo <theo@commerceguys.com>
2012-08-09 12:01:54 +0200

    Issue #1717588 : Attach the enter key to trigger the search on kickstart advanced help
    Change-Id: Ic2d6874b1eb154b74aeb7da7e7bee618c56d47c6
    Reviewed-on: https://code.drupalcommerce.org/300
    Tested-by: Jenkins <jenkins@commerceguys.com>
    Reviewed-by: Bojan Zivanovic <bojan@commerceguys.com>

--- modules/commerce_kickstart/commerce_kickstart_help/commerce_kickstart_help.js
+++ modules/commerce_kickstart/commerce_kickstart_help/commerce_kickstart_help.js
@@ -6,6 +6,15 @@
         $('#edit-submit, #edit-text').attr('disabled', true);
         $('#edit-text').attr('placeholder', "You are not able to search DrupalCommerce.org while offline.");
       }
+      $('#edit-text').bind('keypress', function (event) {
+        var c = event.which ? event.which : event.keyCode;
+        if (c == 13) {
+          event.preventDefault();
+          var e = document.createEvent('MouseEvents');
+          e.initEvent('click', true, true );
+          $('#edit-submit')[0].dispatchEvent(e);
+        }
+      });
       $('#edit-submit').bind('click', function (event) {
         var dc_search_url = 'http://www.drupalcommerce.org/search/node/';
         var search = $('#edit-text').val().trim();
@@ -16,7 +25,7 @@
         search = encodeURI(search).replace('%2f', '/');
         var url = dc_search_url + search;
         $(this).attr('href', url);
-      })
+      });
     }
   }
 })(jQuery);
