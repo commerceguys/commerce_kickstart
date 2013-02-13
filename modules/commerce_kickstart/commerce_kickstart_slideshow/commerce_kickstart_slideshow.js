d003fc23f21a63b8432fcb0b4152bff9f009001e
Mickael Mao <mickael@commerceguys.com>
2013-02-13 15:33:53 +0100

    Update library slideshow.
    Change-Id: If5e39c2e164095884f2e8516b22ec9f216075975
    Reviewed-on: https://code.drupalcommerce.org/630
    Tested-by: JenkinsCG <jenkins@drupalcommerce.org>
    Reviewed-by: Jonathan Sacksick <jonathan.sacksick@gmail.com>

--- modules/commerce_kickstart/commerce_kickstart_slideshow/commerce_kickstart_slideshow.js
+++ modules/commerce_kickstart/commerce_kickstart_slideshow/commerce_kickstart_slideshow.js
@@ -14,20 +14,28 @@
           pause: 5000,
           hideControlOnEnd: false,
           mode: 'fade',
+          pager: false,
           prevText: '<span class="control">' + $('.event-slider .views-row:last .views-field-nothing').html() + '</span>',
           nextText: '<span class="control">' + $('.event-slider .views-row-2 .views-field-nothing').html() + '</span>',
-          onBeforeSlide: function(currentSlideNumber, totalSlideQty, currentSlideHtmlObject){
-            var leftSlideNumber = currentSlideNumber == 0 ? (totalSlideQty - 1) : (currentSlideNumber - 1);
-            var rightSlideNumber = currentSlideNumber == (totalSlideQty - 1) ? 0 : (currentSlideNumber + 1);
-            var leftSlideText = $(currentSlideHtmlObject).parents('.event-slider').find('.views-row-' + (leftSlideNumber + 1) + ':first .views-field-nothing').html();
-            var rightSlideText = $(currentSlideHtmlObject).parents('.event-slider').find('.views-row-' + (rightSlideNumber + 1) + ':first .views-field-nothing').html();
-            $(currentSlideHtmlObject).parents('.bx-wrapper').find('a.bx-prev span').html(leftSlideText);
-            $(currentSlideHtmlObject).parents('.bx-wrapper').find('a.bx-next span').html(rightSlideText);
+          onSlideBefore: function($slideElement, oldIndex, newIndex) {
+            Drupal.behaviors.commerce_kickstartslideshow_custom.processSlide(newIndex, slider.getSlideCount(), $slideElement);
+          },
+          onSliderLoad: function(currentIndex) {
+            var $slideElement = $($('.event-slider li', context)[currentIndex]);
+            Drupal.behaviors.commerce_kickstartslideshow_custom.processSlide(currentIndex, slider.getSlideCount(), $slideElement);
           },
           speed: 400
         });
         $('.event-slider', context).addClass('pager-processed')
       };
+    },
+    processSlide: function(currentSlideNumber, totalSlideQty, currentSlideHtmlObject){
+      var leftSlideNumber = currentSlideNumber == 0 ? (totalSlideQty - 1) : (currentSlideNumber - 1);
+      var rightSlideNumber = currentSlideNumber == (totalSlideQty - 1) ? 0 : (currentSlideNumber + 1);
+      var leftSlideText = $(currentSlideHtmlObject).parents('.event-slider').find('.views-row-' + (leftSlideNumber + 1) + ':first .views-field-nothing').html();
+      var rightSlideText = $(currentSlideHtmlObject).parents('.event-slider').find('.views-row-' + (rightSlideNumber + 1) + ':first .views-field-nothing').html();
+      $(currentSlideHtmlObject).parents('.bx-wrapper').find('a.bx-prev span').html(leftSlideText);
+      $(currentSlideHtmlObject).parents('.bx-wrapper').find('a.bx-next span').html(rightSlideText);
     }
   }
 
